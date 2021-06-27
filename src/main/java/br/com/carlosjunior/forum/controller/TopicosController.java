121d60a77b5624a969e5b83a89f295b42e37cc08
Carlos Cazelatto Junior <carlosjunior1983@gmail.com>
2021-06-27 16:36:24 -0300

    Início da implementacao do GET Topico

--- src/main/java/br/com/carlosjunior/forum/controller/TopicosController.java
+++ src/main/java/br/com/carlosjunior/forum/controller/TopicosController.java
@@ -3,20 +3,32 @@ package br.com.carlosjunior.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.carlosjunior.forum.dto.TopicoDto;
 import br.com.carlosjunior.forum.modelo.Curso;
 import br.com.carlosjunior.forum.modelo.Topico;
+import br.com.carlosjunior.forum.repository.TopicoRepository;
 
 @RestController
+@RequestMapping("/topicos")
 public class TopicosController {
 
-	@RequestMapping("/topicos")
-	public List<TopicoDto> listar() {
-		Topico topico = new Topico("Dúvidas", "Dúvida com Spring", new Curso("SpringBoot", "Programação"));
-		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+	@Autowired
+	private TopicoRepository topicoRepository;
+
+	@GetMapping
+	public List<TopicoDto> listar(String nomeCurso) {
+		if (nomeCurso == null) {
+			List<Topico> topicos = topicoRepository.findAll();
+			return TopicoDto.converter(topicos);
+		}else {
+			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+			return TopicoDto.converter(topicos);
+	}
 	}
 
 }
