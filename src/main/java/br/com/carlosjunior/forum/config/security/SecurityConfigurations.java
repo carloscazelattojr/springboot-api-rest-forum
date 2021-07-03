1df1d89be15f36f9af128f21e2c4691f42727524
Carlos Cazelatto Junior <carlosjunior1983@gmail.com>
2021-07-03 15:47:43 -0300

    Liberando enderecos no Spring Security

--- src/main/java/br/com/carlosjunior/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/carlosjunior/forum/config/security/SecurityConfigurations.java
@@ -1,6 +1,10 @@
 package br.com.carlosjunior.forum.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,6 +12,24 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
-	
-	
+	// Confirugações de autenticação!
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+	}
+
+	// Configurações de autorização
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET, "/topicos").permitAll()
+			.antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+	}
+
+	// Confirgurações de arquivos estáticos( js, css, etc.)
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+
+	}
+
 }
