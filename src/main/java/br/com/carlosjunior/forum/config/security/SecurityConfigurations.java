d4bd49a77c7a5e48d913e674ee6d7cb35ac524d8
Carlos Cazelatto Junior <carlosjunior1983@gmail.com>
2021-07-05 23:23:55 -0300

    Ajsutes de Roles

--- src/main/java/br/com/carlosjunior/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/carlosjunior/forum/config/security/SecurityConfigurations.java
@@ -45,10 +45,10 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.antMatchers(HttpMethod.POST, "/auth").permitAll()
-			.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
 			.antMatchers(HttpMethod.GET, "/topicos").permitAll()
 			.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+			.antMatchers(HttpMethod.POST, "/auth").permitAll()
+			.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
 			.antMatchers(HttpMethod.DELETE, "/topicos/*").hasRole("MODERADOR")
 			.anyRequest().authenticated()
 			.and().csrf().disable()
